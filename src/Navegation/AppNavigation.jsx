707548a88e0c86d237b8dc568e5487ff96483330
juliancastillo21 <oscarjuliancastillo@gmail.com>
2024-03-23 13:33:16 -0500

    Se agrega el proyecto appnavegation

---
+++ src/Navegation/AppNavigation.jsx
@@ -0,0 +1,10 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/native-stack';
+
+const Tab = createBottomTabNavigator();
+
+const Home = createStackNavigator();
+const Offers = createStackNavigator();
+const Buy = createStackNavigator();
+const Credit = createStackNavigator();
+const Sure = createStackNavigator();
--- src/Screens/Credit.jsx
+++ src/Screens/Credit.jsx
@@ -1,12 +1,55 @@
-import React from 'react'
-import { View } from 'react-native'
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Text,Button, TextInput } from 'react-native-paper';
+import { useNavigation } from '@react-navigation/native';
 
 const Credit = () => {
+  const navigation = useNavigation(); 
   return (
-    <View>
-      
+    <View style={styles.container}>
+      <Text style={styles.title}>Crédito para tu carro</Text>
+      <Text style={styles.label}>Monto deseado ($)</Text>
+      <TextInput
+        keyboardType="numeric"
+        style={styles.input}
+      />
+      <Text style={styles.info}>
+        Ingresa el monto de credito que deseas solicitar.
+        La solicitud de credito se enviara automaticamente al ingresar el monto.
+      </Text>
+      {navigation && (
+        <Button icon="" mode="contained" onPress={() => navigation.navigate('Sure')}>
+          Ir a la Siguiente Pantalla
+        </Button>
+      )}
     </View>
-  )
-}
+  );
+};
 
-export default Credit
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  label: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 5,
+  },
+  input: {
+    marginBottom: 20,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    padding: 5,
+  },
+  info: {
+    fontSize: 16,
+  },
+});
+
+export default Credit;
